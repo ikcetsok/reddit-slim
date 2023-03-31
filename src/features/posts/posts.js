5def711a9481d9819b1911364d8f3c81fc53fb65
ikcetsok <panicz.kostek@gmail.com>
2023-03-31 13:41:31 +0200

    start on postsSlice

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -1,1 +1,16 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { selectPosts, loadPosts } from "./postsSlice";
+
+
+export const Posts = () => {
+
+const posts = useSelector(selectPosts);
+const dispatch = useDispatch();
+
+
+
+
+    
+}
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,13 +1,41 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+export const loadPosts = createAsyncThunk(
+  "posts/loadPosts",
+  async (subreddit) => {
+    const posts = await fetch("./subkindafunny.json");
+    const response = await posts.json();
+    return response.children;
+  }
+);
+
 const initialState = {
-    posts:[],
-    isLoading: false,
-    hasError: false
+  posts: [],
+  isLoading: false,
+  hasError: false,
 };
 
 const postSlice = createSlice({
-    name:'posts',
-    initialState: initialState,
-    
-})
+  name: "posts",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loadPosts.pending, (state, action) => {
+      state.isLoading = true;
+      state.hasError = false;
+    });
+    builder.addCase(loadPosts.hasError, (state, action) => {
+      state.isLoading = false;
+      state.hasError = true;
+    });
+    builder.addCase(loadPosts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.hasError = false;
+      state.posts = action.payload;
+    });
+  },
+});
+
+export const selectPosts = (state) => state.posts.posts;
+
+export default postSlice.reducer;
