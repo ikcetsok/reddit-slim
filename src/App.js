d47b734e7eeddc329f57f4ec4ae84f755cf4d710
ikcetsok <panicz.kostek@gmail.com>
2023-04-11 08:55:28 +0200

    navbar prepare for searchbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+// Components
+import { NavBar } from "./components/navbar";
+
+// Features
 import Posts from "./features/posts/posts";
+
+// CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <NavBar/>
         <Posts />
-      </header>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./navbar.css";
+
+export const NavBar = () => {
+  return (<nav className="NavBar">
+    <img src="/logo.jpeg" alt="logo"/>
+    <div>Blablabla</div>
+  </nav>);
+};
