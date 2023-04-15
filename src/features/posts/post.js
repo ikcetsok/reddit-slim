fd4a0dc87b47c5a1c635b607f264220bcaa6632b
ikcetsok <panicz.kostek@gmail.com>
2023-04-15 18:25:18 +0200

    update post css

--- src/features/posts/post.js
+++ src/features/posts/post.js
@@ -1,16 +1,24 @@
 import React from "react";
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser from "react-html-parser";
+import "./post.css";
 
-export function Post ({post}) {
-    const data = post.data;
-    const title = data.title;
-    const content = data.selftext_html;
-   
+export function Post({ post }) {
+  const data = post.data;
+  const title = data.title;
+  const content = data.selftext_html;
+  const author = data.author;
 
-    return (
-        <div className='post'>
-            <h1>{title}</h1>
-            <div className='content' dangerouslySetInnerHTML={{__html: ReactHtmlParser(content)}} />
-        </div>
-    )
+  return (
+    <div className="post">
+        <div></div>
+      <div>
+        <p className="author">Posted by {author}</p>
+        <h1 className="title">{title}</h1>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: ReactHtmlParser(content) }}
+        />
+      </div>
+    </div>
+  );
 }
