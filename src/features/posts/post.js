c244ecca0d802583e8eafc2b5c65fc43fffed793
ikcetsok <panicz.kostek@gmail.com>
2023-04-16 14:34:10 +0200

    loading screen correct

--- src/features/posts/post.js
+++ src/features/posts/post.js
@@ -24,6 +24,7 @@ export function Post({ post }) {
           dangerouslySetInnerHTML={{ __html: ReactHtmlParser(content) }}
         />
       </div>
+      
     </div>
   );
 }
--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -3,20 +3,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectPosts, loadPosts } from "./postsSlice";
 import { Post } from "./post.js";
 
+
 export default function Posts() {
   const posts = useSelector(selectPosts);
-  const dispatch = useDispatch();
-  const element = posts.posts[0]?.data?.author;
-  const turn = () => {
-    dispatch(loadPosts());
-  };
-
-  console.log("posty", posts);
 
   return (
     <section>
       {posts.isLoading ? (
-        <h1>Loading...</h1>
+        <h1> <i class="fas fa-spinner fa-spin"></i>  Loading...</h1>
       ) : (
         posts.posts.slice(0, 5).map((post) => {
           return <Post post={post}/>
