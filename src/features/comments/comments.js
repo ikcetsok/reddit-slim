4fe3520410e41d4d6e0a557cb0e4a724d6ec1f9a
ikcetsok <panicz.kostek@gmail.com>
2023-04-26 18:07:01 +0200

    add

--- src/features/comments/comments.js
+++ src/features/comments/comments.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import { selectComments } from "./commentsSlice";
 import { loadComments } from "./commentsSlice";
 import { resetComments } from "./commentsSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { Comment } from "./comment";
-import './comments.css'
+import "./comments.css";
 
 export const Comments = ({ permalink, num_comments, name }) => {
+  const [buttonSwitch, setButtonSwitch] = useState(true);
   const comments = useSelector(selectComments);
   const dispatch = useDispatch();
   let show = false;
@@ -19,14 +21,24 @@ export const Comments = ({ permalink, num_comments, name }) => {
     <div className="Comments">
       <div className="loadCommentsButton">
         <button
+          className="commentButton"
           onClick={() => {
-            dispatch(loadComments(permalink));
+            if (buttonSwitch) {
+              setButtonSwitch(false);
+              dispatch(loadComments(permalink));
+            } else {
+              setButtonSwitch(true);
+              dispatch(resetComments());
+            }
           }}
         >
-          Comments {num_comments}
+          <p></p> <p>Comments {num_comments}</p>{" "}
+          <p>
+            <i class="fa-regular fa-caret-down"></i>
+          </p>
         </button>
       </div>
-      {comments.isLoading && show && (
+      {comments.isLoading && (
         <>
           <h1 className="LoadingComment">
             <i class="fas fa-spinner fa-spin"></i> Loading...
@@ -38,7 +50,8 @@ export const Comments = ({ permalink, num_comments, name }) => {
           return <Comment comment={comment} />;
         })}
       {show && (
-        <button className="closeComments"
+        <button
+          className="closeComments"
           onClick={() => {
             dispatch(resetComments());
           }}
--- src/features/feeds/feedsSlice.js
+++ src/features/feeds/feedsSlice.js
@@ -14,3 +14,13 @@ export const selectFeeds = (state) => state.feeds.feeds;
 
 export default feedsSlice.reducer;
 
+// import React from 'react';
+
+// function Parent() {
+//   return (
+//     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '300px', height: '50px', border: '1px solid black' }}>
+//       <div style={{ textAlign: 'center' }}>Centered child</div>
+//       <div style={{ marginLeft: 'auto' }}>Child on far right</div>
+//     </div>
+//   );
+// }
