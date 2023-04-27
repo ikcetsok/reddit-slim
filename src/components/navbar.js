8ae08090a9ba6005b9f99ed315911041e8bf2621
ikcetsok <panicz.kostek@gmail.com>
2023-04-27 08:11:58 +0200

    first changes for responsive design

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import { loadPosts } from "../features/posts/postsSlice";
+import { useSelector, useDispatch } from "react-redux";
 import "./navbar.css";
 import { SearchBar } from "../features/searchbar/searchbar";
+import { selectFeeds } from "../features/feeds/feedsSlice";
 
 export const NavBar = () => {
-  return (<nav className="NavBar">
-    <img src="/logo.jpeg" alt="logo"/>
-    <div className="SearchBar"><SearchBar/></div>
-  </nav>);
+  const [subreddit, setSubreddit] = useState(0);
+
+  const subreddits = useSelector(selectFeeds);
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target }) => {
+    if(target.value==0) return ;
+    setSubreddit(target.value);
+    dispatch(loadPosts(target.value));
+  };
+
+  return (
+    <nav className="NavBar">
+      <div className="wideNav">
+      <img src="/logo.jpeg" alt="logo" />
+      <div className="SearchBar">
+        <SearchBar />
+      </div>
+      </div>
+      <div className="dropdownList">
+        <select value={subreddit} onChange={handleChange}>
+          <option value={0}> </option>
+          {subreddits.map((sub) => (
+            <option key={sub} value={sub}>
+              {sub}
+            </option>
+          ))}
+        </select>
+        </div>
+      
+    </nav>
+  );
 };
